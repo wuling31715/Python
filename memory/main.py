884946a2e19bc673178baef3899ed84f5d2e60bb
wuling31715 <wuling31715@gmail.com>
2019-08-20 01:04:40 +0800

    memory

---
+++ memory/main.py
@@ -0,0 +1,23 @@
+import os, time, getpass
+
+def count_down(second):
+    for i in range(second):
+        print(second - i, end=' ', flush=True)
+        time.sleep(1)
+    print()
+
+def sudo_purge(password):
+    command = 'sudo purge'
+    os.system('echo %s|sudo -S %s' % (password, command))
+
+def main():
+    password = getpass.getpass('password: ')
+    counter = 0
+    while True:
+        sudo_purge(password)
+        counter += 1
+        print('sudo purge: %d' % counter)
+        count_down(300)
+
+if __name__ == "__main__":
+    main()
